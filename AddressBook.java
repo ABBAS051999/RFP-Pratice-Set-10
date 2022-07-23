56c7ce9811947a82be262eb3c07bcb064dfd6650
ABBAS051999 <abbas.bsc786@gmail.com>
2022-07-23 18:48:26 +0530

    Added/RFP-Pratice-Set-10

---
+++ AddressBook.java
@@ -0,0 +1,12 @@
+public class AddressBookSystem {
+    // main method
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+
+        AddressBook addressBook = new AddressBook();
+        addressBook.addContact();
+        addressBook.displayContact();
+        addressBook.editContact();
+        addressBook.deleteContact();
+    }
+}
